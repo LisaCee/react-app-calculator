d33641ffc47ae193698322e11f63126a0bd33392
darthMinimus <irishseanvaldivia@gmail.com>
2019-04-18 16:20:43 -0500

    Basic keyboard layout setup

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React from 'react'
 
 class Keyboard extends React.Component {
+  render () {
+    return (
+      <div>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[0])}>{this.props.keys[0]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[1])}>{this.props.keys[1]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[2])}>{this.props.keys[2]}</button>
+        <button onClick={() => this.props.onSignKey('+')}>+</button>
+        <br />
 
-    render() {
-        return (
-            <div>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <br/>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[3])}>{this.props.keys[3]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[4])}>{this.props.keys[4]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[5])}>{this.props.keys[5]}</button>
+        <button onClick={() => this.props.onSignKey('-')}>-</button>
+        <br />
 
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <br/>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[6])}>{this.props.keys[6]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[7])}>{this.props.keys[7]}</button>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[8])}>{this.props.keys[8]}</button>
+        <button onClick={() => this.props.onSignKey('*')}>x</button>
+        <br />
 
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <br/>
+        <button onClick={() => this.props.onKeyPress(this.props.keys[9])}>{this.props.keys[9]}</button>
+        <button onClick={() => this.props.onKeyPress('.')}>.</button>
+        <button onClick={() => this.props.onEqualsKey()}>=</button>
+        <button onClick={() => this.props.onSignKey('/')}>/</button>
+        <br />
 
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <br/>
-
-                <button></button>
-            </div>
-        )
-    }
+        <button onClick={() => this.props.onClearKey()}>CLEAR</button>
+      </div>
+    )
+  }
 }
