147bc15a8dd4a5dada94ce85652bc079e8a1f975
darthMinimus <irishseanvaldivia@gmail.com>
2019-04-20 16:03:33 -0500

    Very fun, starting to really look like a real-life app. There are some more bugs to work out, and I have yet to successfully render the buttons as circles.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default App;
 
 // Exploring like this is fun, but I need to get 'er done
 // I think I'm going to put each row of buttons in its own little flexbox div
-// and then see where we can go from there. 
+// and then see where we can go from there. But darn, that seems convoluted--there has to be a better way
 
 // okay so we're gonna try out CSS Grid
 // yarn upgrade caniuse-lite browserslist yeah we gots to do that; check it out at the leastest
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -5,32 +5,39 @@ class Keyboard extends React.Component {
   render () {
     return (
       <div className="keyboard">
-        <button className="button" onClick={() => this.props.onClearKey()}>C</button>
-        <button className="button" onClick={() => this.props.onPlusMinusKey()}>+/-</button>
-        <button className="button">%</button>
-        <button className="button" onClick={() => this.props.onSignKey('/')}>&divide;</button>
-        <br />
+        <div className="row">
+          <button className="button lt-grey" onClick={() => this.props.onClearKey()}>C</button>
+          <button className="button lt-grey" onClick={() => this.props.onPlusMinusKey()}>+/-</button>
+          <button className="button lt-grey">%</button>
+          <button className="button orng" onClick={() => this.props.onSignKey('/')}>&divide;</button>
+        </div>
 
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[6])}>{this.props.keys[6]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[7])}>{this.props.keys[7]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[8])}>{this.props.keys[8]}</button>
-        <button className="button" onClick={() => this.props.onSignKey('*')}>x</button>
-        <br />
+        <div className="row">
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[6])}>{this.props.keys[6]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[7])}>{this.props.keys[7]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[8])}>{this.props.keys[8]}</button>
+          <button className="button orng" onClick={() => this.props.onSignKey('*')}>x</button>
+        </div>
 
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[4])}>{this.props.keys[4]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[5])}>{this.props.keys[5]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[6])}>{this.props.keys[6]}</button>
-        <button className="button" onClick={() => this.props.onSignKey('-')}>-</button>
-        <br />
+        <div className="row">
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[4])}>{this.props.keys[4]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[5])}>{this.props.keys[5]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[6])}>{this.props.keys[6]}</button>
+          <button className="button orng" onClick={() => this.props.onSignKey('-')}>-</button>
+        </div>
+ 
+        <div className="row">
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[0])}>{this.props.keys[0]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[1])}>{this.props.keys[1]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[2])}>{this.props.keys[2]}</button>
+          <button className="button orng" onClick={() => this.props.onSignKey('+')}>+</button>
+        </div>
 
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[0])}>{this.props.keys[0]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[1])}>{this.props.keys[1]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[2])}>{this.props.keys[2]}</button>
-        <button className="button" onClick={() => this.props.onSignKey('+')}>+</button>
-        <br />
-        <button className="button" onClick={() => this.props.onKeyPress(this.props.keys[9])}>{this.props.keys[9]}</button>
-        <button className="button" onClick={() => this.props.onKeyPress('.')}>.</button>
-        <button className="button" onClick={() => this.props.onEqualsKey()}>=</button>
+        <div className="row last-row">
+          <button className="button zero-btn" onClick={() => this.props.onKeyPress(this.props.keys[9])}>{this.props.keys[9]}</button>
+          <button className="button" onClick={() => this.props.onKeyPress('.')}>.</button>
+          <button className="button orng" onClick={() => this.props.onEqualsKey()}>=</button>
+        </div>
       </div>
     )
   }
